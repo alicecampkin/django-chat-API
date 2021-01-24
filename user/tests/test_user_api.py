b71afe3d37c43773d34a85263d8ef08cc36b7544
Alice Campkin <alice.campkin@gmail.com>
2021-01-24 17:38:12 +0000

    adds tests for registration via API

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -8,20 +8,20 @@ from rest_framework import status
 USER_MODEL = get_user_model()
 
 
-class TestUserAPI(TestCase):
+class TestAPICreateUser(TestCase):
 
     @classmethod
     def setUpTestData(cls):
         cls.client = APIClient()
-        cls.create_user_url = reverse('register')
+        cls.create_user_url = reverse("register")
 
     def test_register_user(self):
         """ Tests we can create a user with a valid payload """
 
         payload = {
-            'email': 'janedoe@test.com',
-            'full_name': 'janedoe',
-            'password': 'password123'
+            "email": "janedoe@test.com",
+            "full_name": "janedoe",
+            "password": "password123"
         }
 
         response = self.client.post(self.create_user_url, payload)
@@ -30,6 +30,49 @@ class TestUserAPI(TestCase):
 
         user = USER_MODEL.objects.get(**response.data)
 
-        self.assertTrue(user.check_password(payload['password']))
+        self.assertTrue(user.check_password(payload["password"]))
+        self.assertNotIn("password", response.data)
 
-        self.assertNotIn('password', response.data)
+    def test_email_uniqueness(self):
+        """ Tests that a registration request is rejected if the email is not unique."""
+
+        user1 = USER_MODEL.objects.create(
+            email="tom@test.com",
+            full_name="Tom Test",
+            password="password123"
+        )
+
+        payload = {
+            "email": "tom@test.com",
+            "full_name": "Tommo Test",
+            "password": "password123"
+        }
+
+        response = self.client.post(self.create_user_url, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_missing_registration_fields(self):
+
+        payloads = [
+            {
+                "email": "",
+                "full_name": "Tommo Test",
+                "password": "password123"
+            },
+            {
+                "email": "tom2@test.com",
+                "full_name": "",
+                "password": "password123"
+            },
+            {
+                "email": "tom3@test.com",
+                "full_name": "Tom Test",
+                "password": ""
+            },
+        ]
+
+        for payload in payloads:
+            response = self.client.post(self.create_user_url, payload)
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+            response = None
